84fd5057338e71b6f10805d21b8de5d9930fa0ce
edentechofc <edentechofc@gmail.com>
2026-06-09 08:42:26 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+export default defineConfig(() => ({
+  server: {
+    host: '::',
+    port: 8080,
+  },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+}));
